86aa01b5c37cfd2170011150cf1678052271b1d9
nikhilyalavarthy07-stack <nikhil.yalavarthy07@gmail.com>
2026-08-19 18:22:29 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -18,7 +18,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
-        "https://ask-my-notes-jw3z.onrender.com"
+        "https://askmynotes-02.onrender.com"
     ],
     allow_credentials=True,
     allow_methods=["*"],
